cccc54786b653f0f724eb332fc40607f380b6d33
Zigmas Satkevičius <Zylius1@gmail.com>
2015-10-27 22:51:42 +0200

    Added basic template for image api controller.

---
+++ src/Galerija/APIBundle/Controller/ImageController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Galerija\APIBundle\Controller;
+
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Routing\ClassResourceInterface;
+use Symfony\Component\HttpFoundation\File\File;
+
+class ImageController extends FOSRestController implements ClassResourceInterface
+{
+    public function putAction(File $file)
+    {
+
+    }
+
+    public function getAction()
+    {
+
+    }
+
+    public function getAllAction()
+    {
+
+    }
+
+    public function deleteAction()
+    {
+
+    }
+
+    public function patchAction()
+    {
+
+    }
+
+}
