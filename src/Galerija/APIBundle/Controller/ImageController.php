5ee63533533a26385ad31cc818994ffc3d67d3ac
Zigmas Satkevičius <Zylius1@gmail.com>
2015-11-02 23:37:03 +0300

    Began work on post controller.

--- src/Galerija/APIBundle/Controller/ImageController.php
+++ src/Galerija/APIBundle/Controller/ImageController.php
@@ -3,7 +3,12 @@
 namespace Galerija\APIBundle\Controller;
 
 use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Request\ParamFetcher;
 use FOS\RestBundle\Routing\ClassResourceInterface;
+use FOS\RestBundle\View\View;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use FOS\RestBundle\Controller\Annotations\RequestParam;
+use Galerija\APIBundle\Entity\Image;
 use Symfony\Component\HttpFoundation\File\File;
 
 class ImageController extends FOSRestController implements ClassResourceInterface
@@ -11,6 +16,39 @@ class ImageController extends FOSRestController implements ClassResourceInterfac
     public function putAction(File $file)
     {
 
+    }
+
+    /**
+     * Create an Image from the submitted data.
+     *
+     * @ApiDoc(
+     *   resource = true,
+     *   description = "Creates a new image from the submitted data.",
+     *   statusCodes = {
+     *     200 = "Returned when successful.",
+     *     400 = "Returned when image wasnt saved successfully."
+     *   }
+     * )
+     *
+     * @param ParamFetcher $paramFetcher Paramfetcher
+     *
+     * @RequestParam(name="title", nullable=false, strict=true, description="Title.")
+     * @RequestParam(name="data", nullable=false, strict=true, description="Image data.")
+     *
+     * @return View
+     */
+    public function postAction(ParamFetcher $paramFetcher)
+    {
+        $image = new Image();
+
+        $imageFile = imagecreatefromstring(base64_decode($paramFetcher->get('data')));
+
+        $image->setImagePath('images/'. uniqid() . '.png');
+        $image->setTitle($paramFetcher->get('title'));
+
+        $imageSaveSuccess = file_put_contents($image->getImagePath(), $imageFile);
+        $view = View::create();
+        return $imageSaveSuccess ? $view->setData($image)->setStatusCode(200) : $view->setStatusCode(400);
     }
 
     public function getAction()
---
+++ src/Galerija/APIBundle/Entity/Image.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Galerija\APIBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Image
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Image
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="imagePath", type="string", length=255)
+     */
+    private $imagePath;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return Image
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set imagePath
+     *
+     * @param string $imagePath
+     *
+     * @return Image
+     */
+    public function setImagePath($imagePath)
+    {
+        $this->imagePath = $imagePath;
+
+        return $this;
+    }
+
+    /**
+     * Get imagePath
+     *
+     * @return string
+     */
+    public function getImagePath()
+    {
+        return $this->imagePath;
+    }
+}
+
